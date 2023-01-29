e2839a6a0b8a3a7cf6bdbbe3f65f8d5afed99b4f
Ran Shamay <ranshamay@microsoft.com>
2023-01-29 11:00:23 +0200

    apply fallback

--- csr_ssr/src/bootstrap.js
+++ csr_ssr/src/bootstrap.js
@@ -5,8 +5,6 @@ import i18n from "./i18n";
 import { initializeIcons } from "@fluentui/react";
 import { ApplicationInsights } from "./mocks/@microsoft/1ds-analytics-web-js";
 import { getAccessTokenCB } from "./utils/RestClient";
-import RestClient from "@core/RestClient";
-import logger from "@core/logger";
 import { v4 } from "uuid";
 
 import "./index.css";
@@ -29,11 +27,26 @@ async function hydrateClient() {
   // initialize app prerequisites before hydration
   const _1dsLogger = new ApplicationInsights();
   _1dsLogger.initialize(loggerConfig, []);
-  logger.init(_1dsLogger, {
+  let logger;
+  let RestClient;
+
+  try {
+    logger = await import("@core/logger");
+  } catch (e) {
+    console.log('failed to load logger', e);
+  }
+  try {
+    RestClient = await import("@core/RestClient");
+  } catch (e) {
+    console.log('failed to load restClient', e);
+  }
+
+  logger?.init(_1dsLogger, {
     correlationId: v4(),
     hostType: "Sample App - CSR_SSR",
   });
-  RestClient.init(mockedLogger, getAccessTokenCB);
+
+  RestClient?.init(mockedLogger, getAccessTokenCB);
   initializeIcons();
   const i18nInstance = await i18n();
   const locale = getLocale();
--- csr_ssr/src/components/OfflineRemote.js
+++ csr_ssr/src/components/OfflineRemote.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React from 'react';
+import { MessageBar, MessageBarType, Stack, Text, IconButton } from '@fluentui/react';
 
 export default function OfflineRemote() {
   return (
-    <div>
-      <h1>Offline Remote</h1>
-    </div>
+    <MessageBar messageBarType={MessageBarType.error} isMultiline>
+      <Stack enableScopedSelectors horizontal horizontalAlign="space-around" verticalAlign="center">
+        <Stack.Item align="auto">
+          <Text>OOPS, Remote Component is offline</Text>
+        </Stack.Item>
+        <Stack.Item align="auto">
+          <IconButton iconProps={{ iconName: 'PlugDisconnected' }} title="Offline Component" ariaLabel="Offline Component" />
+        </Stack.Item>
+      </Stack>
+    </MessageBar>
   );
 }
